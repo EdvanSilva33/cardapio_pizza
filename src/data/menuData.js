f220e092446216efb2c6e517c893334532a14e0e
Edvan Silva <edvansilvapereira@gmail.com>
2025-09-27 18:19:03 -0300

    ADD BEBIDAS

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -21,7 +21,7 @@ export const menuData = {
           pequena: { price: 52.90, description: "4 pedaços" },
           grande: { price: 73.90, description: "8 pedaços" }
         },
-        image: "/src/assets/Margherita.png",
+        image: "/img/Margherita.png",
         category: "pizza"
       },
        {
@@ -29,8 +29,8 @@ export const menuData = {
         name: "MOZZARELLA",
         description: "Molho de tomate, mussarela e orégano",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 48.90, description: "4 pedaços" },
+          grande: { price: 67.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -40,8 +40,8 @@ export const menuData = {
         name: "PRESUNTO",
         description: "Molho de tomate, mussarela, presunto, cebola, pimentão e orégano.",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 53.90, description: "4 pedaços" },
+          grande: { price: 74.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -51,8 +51,8 @@ export const menuData = {
         name: "NAPOLITANA",
         description: "  Molho de tomate, mussarela, aliche, tomate cereja e orégano.",
         sizes: {
-          pequena: { price: 36.00, description: "4 pedaços" },
-          grande: { price: 46.00, description: "8 pedaços" }
+          pequena: { price: 62.90, description: "4 pedaços" },
+          grande: { price: 83.90, description: "8 pedaços" }
         },
         image: "/src/assets/Napolitana.png",
         category: "pizza"
@@ -60,10 +60,10 @@ export const menuData = {
       {
         id: 6,
         name: "FRANGO E CATUPIRY",
-        description: "Molho de tomate, muçarela , frango desfiado, catupiry e orégano.",
+        description: "Molho de tomate, mussarela , frango desfiado, catupiry e orégano.",
         sizes: {
-          pequena: { price: 38.00, description: "4 pedaços" },
-          grande: { price: 48.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/pizza-calabresa.jpg", // Não encontrei imagem específica, mantendo placeholder
         category: "pizza"
@@ -73,8 +73,8 @@ export const menuData = {
         name: "PORTUGUESA",
         description: "Molho de tomate, mussarela, ovos cozidos, presunto, cebola, pimentão e azeitonas.",
         sizes: {
-          pequena: { price: 42.00, description: "4 pedaços" },
-          grande: { price: 52.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Portuguesa.png",
         category: "pizza"
@@ -82,10 +82,10 @@ export const menuData = {
         {
         id: 8,
         name: "CALABRESA",
-        description: "Molho de tomate, mussarela, linguiça calabresa,cebola e azeitonas",
+        description: "Molho de tomate, mussarela, linguiça calabresa, cebola e azeitonas",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -95,8 +95,8 @@ export const menuData = {
         name: "PARMEGIANA",
         description: "Molho de tomate, mussarela,, berinjela, parmesão e manjericão.",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -104,10 +104,10 @@ export const menuData = {
         {
         id: 10,
         name: "ATUM PREMIUM",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Molho de tomate, mussarela , atum, cebola e orégano",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -115,10 +115,10 @@ export const menuData = {
         {
         id: 11,
         name: "CHAMPIGNON",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Molho de tomate, mussarela, champignons Paris fatia, orégano.",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 53.90, description: "4 pedaços" },
+          grande: { price: 74.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -126,10 +126,10 @@ export const menuData = {
         {
         id: 12,
         name: "VEGETARIANA ESPECIAL",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Molho de tomate, mussarela, parmesão, champignon, abobrinha, tomate cereja e orégano",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -137,10 +137,10 @@ export const menuData = {
         {
         id: 13,
         name: "LOMBO COM CATUPIRY",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Molho de tomate, mussarela, lombo defumado ecatupiry",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -148,10 +148,10 @@ export const menuData = {
         {
         id: 14,
         name: "PEPPERONI",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Pomodoro pelati, mussarela, peperoni e orégano",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 53.90, description: "4 pedaços" },
+          grande: { price: 74.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -159,10 +159,10 @@ export const menuData = {
         {
         id: 15,
         name: "3 QUEIJOS",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Pomodoro pelate, mussarela, gorgonzola e granapadano",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -170,10 +170,10 @@ export const menuData = {
         {
         id: 16,
         name: "MARGUERITA ESPECIAL",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Pomodoro pelate, mussarela, burrata, granapadano e manjericão",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -181,20 +181,20 @@ export const menuData = {
         {
         id: 17,
         name: "CAPRICIOSA",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Pomodoro pelate, presunto cozido, coração alcachofra, champignon e bacon",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
       },  {
         id: 18,
         name: "TOMATE SECO E RÚCULA",
-        description: "Molho de tomate, mussarela e oregano",
+        description: "Pomodoro pelate, mussarela, tomate seco e rúcula ",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 56.90, description: "4 pedaços" },
+          grande: { price: 77.90, description: "8 pedaços" }
         },
         image: "/src/assets/Mozzarella.png",
         category: "pizza"
@@ -208,7 +208,7 @@ export const menuData = {
       {
         id: 19,
         name: "ABOBRINHA E CAPRINO",
-        description: "Molho, cream cheese, bacon, azeitona, mussarela e oregano",
+        description: "Molho de tomate, muçarela, parmesão, queijocaprino e abobrinha",
         sizes: {
           pequena: { price: 62.90, description: "4 pedaços" },
           grande: { price: 83.90, description: "8 pedaços" }
@@ -219,7 +219,7 @@ export const menuData = {
       {
         id: 20,
         name: "CAPRESE",
-        description: "Molho de tomate, mussarela, gorgonzola, parmesão e catupiry",
+        description: "Molho de tomate, mussarela, mussarela de búfala, tomate italiano, pesto de azeitonas pretas e manjericão",
         sizes: {
           pequena: { price: 62.90, description: "4 pedaços" },
           grande: { price: 83.90, description: "8 pedaços" }
@@ -230,7 +230,7 @@ export const menuData = {
       {
         id: 21,
         name: "TOSCANA PIMENTINHA",
-        description: "Molho de tomate, frango desfiado, catupiry e oregano",
+        description: " Molho de tomate, muçarela, mussarela de búfala, tomate italiano, pesto de azeitonas pretas e manjericão",
         sizes: {
           pequena: { price: 61.90, description: "4 pedaços" },
           grande: { price: 82.90, description: "8 pedaços" }
@@ -248,26 +248,26 @@ export const menuData = {
         name: "BANANA E CANELA",
         description: "Massa doce, banana, canela e açúcar cristal",
         sizes: {
-          pequena: { price: 25.00, description: "4 pedaços" },
-          grande: { price: 35.00, description: "8 pedaços" }
+          pequena: { price: 52.90, description: "4 pedaços" },
+          grande: { price: 82.90, description: "8 pedaços" }
         },
         image: "/src/assets/pizza-chocolate.jpg", // Não encontrei imagem específica, mantendo placeholder
         category: "pizza"
       },
       {
-        id: 10,
-        name: "Pizza de Brigadeiro",
-        description: "Massa doce, brigadeiro e granulado colorido",
+        id: 23,
+        name: "CHOCOLATE E MORANGO",
+        description: "mussarela, calda de chocolate ao leite e morango",
         sizes: {
-          pequena: { price: 28.00, description: "4 pedaços" },
-          grande: { price: 38.00, description: "8 pedaços" }
+          pequena: { price: 61.90, description: "4 pedaços" },
+          grande: { price: 82.90, description: "8 pedaços" }
         },
         image: "/src/assets/pizza-brigadeiro.jpg", // Não encontrei imagem específica, mantendo placeholder
         category: "pizza"
       },
       {
-        id: 11,
-        name: "Pizza de Banana com Canela",
+        id: 24,
+        name: "NUTELLA.",
         description: "Massa doce, banana, canela e açúcar cristal",
         sizes: {
           pequena: { price: 22.00, description: "4 pedaços" },
@@ -277,12 +277,12 @@ export const menuData = {
         category: "pizza"
       },
       {
-        id: 12,
+        id: 25,
         name: "Pizza de Nutella",
-        description: "Massa doce, nutella e morangos",
+        description: "Calda de nutella e avelãs raladas",
         sizes: {
-          pequena: { price: 32.00, description: "4 pedaços" },
-          grande: { price: 42.00, description: "8 pedaços" }
+          pequena: { price: 61.90, description: "4 pedaços" },
+          grande: { price: 82.90, description: "8 pedaços" }
         },
         image: "/src/assets/pizza-nutella.jpg", // Não encontrei imagem específica, mantendo placeholder
         category: "pizza"
@@ -293,43 +293,171 @@ export const menuData = {
     title: "Bebidas",
     items: [
       {
-        id: 13,
+        id: 26,
         name: "Coca-Cola 2L",
         description: "Refrigerante Coca-Cola 2 litros",
-        price: 12.00,
+        price: 12.99,
         image: "/src/assets/Coca-Cola2l.png",
         category: "bebida"
       },
-      {
-        id: 14,
-        name: "Guaraná Antarctica 2L",
+          {
+        id: 27,
+        name: "COCA-COLA ZERO 2L",
+        description: "Refrigerante Coca-Cola Zero 2 litros",
+        price: 12.99,
+        image: "/src/assets/Coca-Cola2l.png",
+        category: "bebida"
+      },
+       {
+        id: 28,
+        name: "Guaraná 2L",
         description: "Refrigerante Guaraná Antarctica 2 litros",
-        price: 10.00,
+        price: 12.99,
+        image: "/src/assets/Guarana-Antarctica-2L.png",
+        category: "bebida"
+      },  {
+        id: 28,
+        name: "Guaraná 2L",
+        description: "Refrigerante Guaraná Antarctica 2 litros",
+        price: 12.99,
         image: "/src/assets/Guarana-Antarctica-2L.png",
         category: "bebida"
       },
+        {
+        id: 29,
+        name: "GUARANÁ ZERO  2L",
+        description: "Refrigerante Guaraná Zero 2 litros",
+        price: 12.99,
+        image: "/src/assets/Guarana-Zero-2L.png",
+        category: "bebida"
+      },
+         {
+        id: 30,
+        name: "FANTA LARANJA 2L",
+        description: "Refrigerante Fanta Laranja 2 litros",
+        price: 12.99,
+        image: "/src/assets/Fanta-Laranja-2L.png",
+        category: "bebida"
+      },
       {
-        id: 15,
-        name: "Suco de Laranja 1L",
+        id: 31,
+        name: "SPRITE 2L",
         description: "Suco natural de laranja 1 litro",
         price: 8.00,
         image: "/src/assets/Suco-de-Laranja-1L.png",
         category: "bebida"
       },
       {
-        id: 16,
-        name: "Água Mineral 500ml",
-        description: "Água mineral sem gás 500ml",
-        price: 3.00,
-        image: "/src/assets/agua-Mineral-500ml.png",
+        id: 32,
+        name: "Água Mineral 300ml",
+        description: "Água mineral sem gás 300ml",
+        price: 5.99,
+        image: "/src/assets/agua-Mineral-300ml.png",
+        category: "bebida"
+      },
+         {
+        id: 33,
+        name: "Água Mineral C/ Gás 300ml",
+        description: "Água mineral sem gás 300ml",
+        price: 5.99,
+        image: "/src/assets/agua-Mineral-300ml.png",
+        category: "bebida"
+      },
+          {
+        id: 34,
+        name: "SUCO DEL VALLE DE UVA 1L",
+        description: "Suco de uva 1 litro",
+        price: 5.99,
+        image: "/src/assets/Suco-Del-Valle-Uva-1L.png",
         category: "bebida"
       },
       {
-        id: 17,
-        name: "Cerveja Heineken 600ml",
-        description: "Cerveja Heineken long neck 600ml",
-        price: 15.00,
-        image: "/src/assets/Cerveja-Heineken-600ml.png",
+        id: 35,
+        name: " HEINEKEN PREMIUM LAGER 330ml",
+        description: "Cerveja Heineken long neck 330ml",
+        price: 11.90,
+        image: "/src/assets/Cerveja-Heineken-330ml.png",
+        category: "bebida"
+      },
+        {
+        id: 36,
+        name: "STELLA ARTOIS PREMIUM LAGER 275ml",
+        description: "Cerveja Stella Artois long neck 275ml",
+        price: 11.90,
+        image: "/src/assets/Cerveja-Stella-Artois-275ml.png",
+        category: "bebida"
+      },
+        {
+        id: 37,
+        name: " BUDWEISER 330ml",
+        description: "Cerveja Budweiser long neck 330ml",
+        price: 11.90,
+        image: "/src/assets/Cerveja-Budweiser-330ml.png",
+        category: "bebida"
+      },
+      ,
+        {
+        id: 38,
+        name: "CORONA EXTRA 330ml",
+        description: "Cerveja Corona Extra long neck 330ml",
+        price: 11.90,
+        image: "/src/assets/Cerveja-Corona-Extra-330ml.png",
+        category: "bebida"
+      },
+          {
+        id: 39,
+        name: "TAPADA DA CUNHA",
+        description: "Vinho Tapada da Cunha",
+        price: 75.00,
+        image: "/src/assets/Vinho-Tapada-da-Cunha.png",
+        category: "bebida"
+      },
+          {
+        id: 40,
+        name: "ROMANICA ROSSO ",
+        description: "Vinho Champu",
+        price: 85.00,
+        image: "/src/assets/Vinho-Champu.png",
+        category: "bebida"
+      },
+          {
+        id: 41,
+        name: "VINAS DEL TANGO",
+        description: "Vinho Vinhas del Tango",
+        price: 72.00,
+        image: "/src/assets/Vinho-Vinhas-del-Tango.png",
+        category: "bebida"
+      },
+      {
+        id: 42,
+        name: "ESPUMANTE FINCA TERRA NOSTRA",
+        description: "Espumante Finca Terra Nostra",
+        price: 61.00,
+        image: "/src/assets/Espumante-Finca-Terra-Nostra.png",
+        category: "bebida"
+      },
+      {
+        id: 43,
+        name: "ESTATE DONOSO",
+        description: "Espumante Estate Donoso",
+        price: 75.00,
+        image: "/src/assets/Espumante-Estate-Donoso.png",
+        category: "bebida"
+      },
+      {
+        id: 44,
+        name: "VINAS DELMAR GRAND RESERVA",
+        description: "Espumante Vinas Delmar Grand Reserva",
+        price: 65.00,
+        image: "/src/assets/Espumante-Vinas-Delmar-Grand-Reserva.png",
+        category: "bebida"
+      },
+      {
+        id: 45,
+        name: "VULCANIC PINOT GRIGIO",
+        description: "Itália",
+        price: 75.00,
+        image: "/src/assets/Cerveja-Loka-Loka-269ml.png",
         category: "bebida"
       }
     ]
@@ -340,6 +468,7 @@ export const categories = [
   { id: "pizzas-tradicionais", name: "Pizzas Tradicionais" },
   { id: "pizzas-especiais", name: "Pizzas Especiais" },
   { id: "pizzas-doces", name: "Pizzas Doces" },
-  { id: "bebidas", name: "Bebidas" }
+  { id: "bebidas", name: "Bebidas" },
+  
 ];
 
